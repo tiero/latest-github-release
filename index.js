9241bfd76547fa821e7d2e1a2ea9324f537b0346
tiero <3596602+tiero@users.noreply.github.com>
2024-02-09 18:19:18 +0100

    require GITHUB_TOKEN ans assetName from query string

--- index.js
+++ index.js
@@ -8,25 +8,31 @@ const app = express();
 const PORT = 3000;
 const GITHUB_ORG = process.env.GITHUB_ORG;
 const GITHUB_REPO = process.env.GITHUB_REPO;
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const ASSET_NAME_FORMAT = process.env.ASSET_NAME_FORMAT;
 
-app.get('/latest-release', async (req, res) => {
+app.get('/latest-release/:name', async (req, res) => {
     try {
-        const response = await fetch(`https://api.github.com/repos/${GITHUB_ORG}/${GITHUB_REPO}/releases/latest`);
+        const headers = {};
+        if (GITHUB_TOKEN) {
+            headers['Authorization'] = `token ${GITHUB_TOKEN}`;
+        }
+
+        const response = await fetch(`https://api.github.com/repos/${GITHUB_ORG}/${GITHUB_REPO}/releases/latest`, {
+            headers: headers
+        });
         if (!response.ok) {
             throw new Error("Failed to fetch the latest release from GitHub.");
         }
         
         const releaseData = await response.json();
-        const version = releaseData.tag_name.slice(1); // Remove 'v' from version, like '0.0.29'
-        
-        const formattedAssetName = ASSET_NAME_FORMAT.replace('{version}', version);
-        const dmgLink = releaseData.assets.find(asset => asset.name === formattedAssetName);
+        const assetName = req.params.name;
+        const dmgLink = releaseData.assets.find(asset => asset.name === assetName);
         
         if (dmgLink && dmgLink.browser_download_url) {
             res.redirect(dmgLink.browser_download_url);
         } else {
-            res.status(404).send(`Asset with name format "${formattedAssetName}" not found in the latest release.`);
+            res.status(404).send(`Asset with name format "${assetName}" not found in the latest release.`);
         }
     } catch (error) {
         console.error(error);
